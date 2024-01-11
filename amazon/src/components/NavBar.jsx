115037e7db255ed3a8c16c5d8bc2671d9f1f4207
ShreeHariesh <shreehariesh29@gmail.com>
2024-01-11 12:39:09 +0530

    new setup

---
+++ amazon/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav>
+            <ul style={{justifyContent:'start', display:'flex'}}>
+                <li style={{textDecoration:'none', listStyle:'none'}}>Main page</li>
+                <li style={{textDecoration:'none', listStyle:'none'}}>Main page</li>
+                <li style={{textDecoration:'none', listStyle:'none'}}>Main page</li>
+                <li style={{textDecoration:'none', listStyle:'none'}}>Main page</li>
+                <li style={{textDecoration:'none', listStyle:'none'}}>Main page</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ amazon/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomeComponent from '../components/HomeComponent'
+function Home() {
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  )
+}
+
+export default Home
